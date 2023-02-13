6804946e48d1e3403c5f8519dd023ef0e3436fc3
Meltem <meltemvurgun@gmail.com>
2023-02-13 17:59:05 -0500

    new commit

---
+++ force-app/main/default/lwc/forEachLoop/forEachLoop.js
@@ -0,0 +1,49 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachLoop extends LightningElement {
+
+    contacts=[
+{
+
+Id: 1,
+Name: 'Meltem Vurgun',
+Title: 'Salesforce Developer'
+
+
+
+},
+
+{
+
+    Id: 2,
+    Name: 'Jhon Carter',
+    Title: 'Salesforce Administrator'
+    
+    
+    
+    },
+
+    {
+
+        Id: 3,
+        Name: 'Jimmy Hanse',
+        Title: 'Salesforce Consaltant'
+        
+        
+        
+        },
+
+        {
+
+            Id: 4,
+            Name: 'Melissa James',
+            Title: 'Salesforce CPQ Developer'
+            
+            
+            
+            }
+
+
+
+    ]
+}
