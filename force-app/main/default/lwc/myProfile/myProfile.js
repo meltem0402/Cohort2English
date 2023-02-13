c4ade017a47b33b545fa34339a36891d1f8ed466
Meltem <meltemvurgun@gmail.com>
2023-02-13 17:16:26 -0500

    New Commit

---
+++ force-app/main/default/lwc/myProfile/myProfile.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class MyProfile extends LightningElement {
+
+    name= 'Meltem Vurgun';
+    title='Salesforce Developer';
+    pickURL = 'https://i.pinimg.com/236x/77/ce/df/77cedfd0efed2a78e742176d5d352c6b--bicycle-decor-bicycle-basket.jpg'
+
+}
